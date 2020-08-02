ed9180bf2140524dc6bade41a37675dea9dd5dd3
zverev-iv <51955637+zverev-iv@users.noreply.github.com>
2020-08-02 13:21:08 +0300

    fix shim

--- cerebro-es/tools/chocolateyinstall.ps1
+++ cerebro-es/tools/chocolateyinstall.ps1
@@ -8,3 +8,5 @@ $packageArgs["packageName"] = $env:ChocolateyPackageName
 $packageArgs["unzipLocation"] = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 Install-ChocolateyZipPackage @packageArgs
+
+Install-BinFile -Name 'cerebro' -Path (Join-Path (Join-Path $toolsDir 'bin') 'cerebro.bat')
