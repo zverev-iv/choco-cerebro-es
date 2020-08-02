1a9ac48f376fa77be430b38548685a229dd15ec7
Ilya Zverev <izverev@logrocon.com>
2020-08-02 14:24:24 +0300

    fix warnings

--- cerebro-es/tools/chocolateyinstall.ps1
+++ cerebro-es/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $meta = Get-Content -Path $env:ChocolateyPackageFolder\tools\packageArgs.json -Raw
 $packageArgs = @{}
-(ConvertFrom-Json $meta).psobject.properties | Foreach { $packageArgs[$_.Name] = $_.Value }
+(ConvertFrom-Json $meta).psobject.properties | ForEach-Object { $packageArgs[$_.Name] = $_.Value }
 
 $packageArgs["packageName"] = $env:ChocolateyPackageName
 $packageArgs["unzipLocation"] = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
